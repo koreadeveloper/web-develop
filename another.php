b37790be4148e445d9b1be5b6f43e7b8f3c5f021
kiminkyu <34590448+koreadeveloper@users.noreply.github.com>
2019-01-23 18:19:20 +0900

    Add files via upload

---
+++ another.php
@@ -0,0 +1,26 @@
+<?php
+/*
+  echo " 3과목의 평균을 구해보아요 ";
+  echo "---------------------------- <br />";
+ 
+  $math = 100;
+  $english = 95;
+  $science = 85;
+ 
+  $sum = $math + $english + $science;       // < ===== $sum이라는 변수를 만들고 값은 3과목의 합이다.
+  $avg = $sum/3;                                           //     3으로나눠서 평균값이 $avg의 값이 된다.
+ 
+  echo " math point : $math <br /> english point : $english <br /> science point : $science <br />";
+  echo "======================== <br />"; 
+  echo " total : $sum <br />";
+  echo " average : $avg <br />"; 
+  echo "======================== <br />";
+  echo " what is the type of \$avg?  <br />";
+  echo gettype($avg);
+  */
+  $num_test = 2147483647;
+  $num_te = 2147483648;
+  echo "$num_test<br>";
+  var_dump($num_test);
+  var_dump($num_te);
+?>
---
+++ test.php
@@ -0,0 +1,28 @@
+<?php
+  echo "helloda";
+  echo "<br>";
+  /*
+  주석
+  */
+  // 주석
+  $num1=33;
+  $num2=33.3;
+  $num3="kiminkyu";
+  echo "\$num1=$num1<br>";
+  echo "\$num2=$num2<br>";
+  echo "\$num3=$num3<br>";
+  echo "세과목의 평균을 구합니다<br>";
+  echo "=======================<br>";
+  $math=100;
+  $english=45;
+  $korean=37;
+  echo "math point:$math<br>";
+  echo "english point:$english<br>";
+  echo "korean poin:$korean<br>";
+  echo "=======================<br>";
+  echo "total:$math+$english+$korean<br>";
+  echo "average:($math+$english+$korean)/3<br>"
+  echo "=======================<br>";
+  echo "What is the type of \$math?";
+  echo gettype($math);
+?>
